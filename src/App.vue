b21f4fccc8f685fd2e03f64e4aeb89c27f050a35
Taninchot Phuwaloetthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-03-31 18:16:42 +0700

    [ADD] First Upload : Udemy EP.23

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <h1>{{title}}</h1>
+    <input type="text" ref="name">
+    <button @click="handleClick">Click Me</button>
+    <Modal />
 </template>
 
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name:'App',
+  components:{ Modal },
+  data() {
+    return {
+      title: 'This is my First App'
+    }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name.value)
+      this.$refs.name.classList.add('active')
+    }
+  },
+
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#app{
+  margin: 0;
 }
 </style>
